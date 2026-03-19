c0ef84b854a17e62fe358cf3e26fcaf707038222
XIANGYU MOU <xiangyu.moe.ac@gmail.com>
2026-03-19 02:06:46 +0000

    refactor: move currency client hooks into modules

--- src/modules/currency/client.ts
+++ src/modules/currency/client.ts
@@ -1,2 +1,2 @@
-export { useConvertedAmount } from "@/features/currency/client/hooks/use-converted-amount";
-export { useAmountDisplay } from "@/features/currency/client/hooks/use-amount-display";
+export { useConvertedAmount } from "./useConvertedAmount";
+export { useAmountDisplay } from "./useAmountDisplay";
