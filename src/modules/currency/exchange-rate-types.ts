3751f19a86418c97d48745d8d419a93447d98845
XIANGYU MOU <xiangyu.moe.ac@gmail.com>
2026-03-21 09:17:48 +0000

    refactor: expose canonical exchange rate module

--- src/modules/currency/exchange-rate-types.ts
+++ src/modules/currency/exchange-rate-types.ts
@@ -1,15 +1,5 @@
-export interface ExchangeRates {
-  base: string;
-  date: string;
-  rates: Record<string, number>;
-}
-
-export interface ExchangeRatesStoredEvent {
-  date: string;
-  base: string;
-  rates: Record<string, number>;
-}
-
-export type ExchangeRatesStoredHandler = (
-  event: ExchangeRatesStoredEvent
-) => void | Promise<void>;
+export type {
+  ExchangeRates,
+  ExchangeRatesStoredEvent,
+  ExchangeRatesStoredHandler,
+} from "./application/services/exchange-rate";
