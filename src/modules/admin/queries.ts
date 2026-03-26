a0d8258ff54e2e36a00639e2fa5ee97daa6fab23
XIANGYU MOU <xiangyu.moe.ac@gmail.com>
2026-03-26 16:17:12 +0800

    feat: add admin entry list query

--- src/modules/admin/queries.ts
+++ src/modules/admin/queries.ts
@@ -3,10 +3,7 @@ export { listAdminTasks } from "./application/queries/list-admin-tasks";
 export { getAdminTaskDetail } from "./application/queries/get-admin-task-detail";
 export { listAdminSourceDocuments } from "./application/queries/list-admin-source-documents";
 export { getAdminSourceDocumentDetail } from "./application/queries/get-admin-source-document-detail";
-
-export async function listAdminEntries(_input: unknown) {
-  throw new Error("listAdminEntries is not implemented yet");
-}
+export { listAdminEntries } from "./application/queries/list-admin-entries";
 
 export async function getAdminEntryDetail(_input: unknown) {
   throw new Error("getAdminEntryDetail is not implemented yet");
