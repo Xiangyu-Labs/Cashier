3cb695532849005dcab4975252866ea46df5dc83
Xiangyu <xiangyu.moe@gmail.com>
2026-07-07 19:00:47 +0800

    refactor: remove password and sso login ui

--- src/modules/auth/ui/index.ts
+++ src/modules/auth/ui/index.ts
@@ -1,6 +1,5 @@
 export { AuthLoginPage } from "./login-page";
 export { ResendCountdown } from "./resend-countdown";
-export { PasswordForm } from "./PasswordForm";
 export { ChangeEmailForm } from "./ChangeEmailForm";
 export { ClearDataForm } from "./ClearDataForm";
 export { DeleteAccountForm } from "./DeleteAccountForm";
